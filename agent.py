90454452070b28aefb8aae25ffeaebe3efb53f5e
DaviOliveiraSilva <davicampinas@gmail.com>
2025-06-26 11:56:40 -0300

    first commit - FL model

---
+++ agent.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+class PolicyNetwork(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.mean = nn.Linear(64, action_dim)
+        self.log_std = nn.Parameter(torch.zeros(action_dim))
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        mean = self.mean(x)
+        std = self.log_std.exp().expand_as(mean)
+        return mean, std
+
+    def act(self, state):
+        mean, std = self.forward(state)
+        dist = torch.distributions.Normal(mean, std)
+        action = dist.sample()
+        log_prob = dist.log_prob(action).sum(dim=-1)
+        return action, log_prob
